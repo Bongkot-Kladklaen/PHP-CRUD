3c583c7aa815b45a9346431e7e22f50ea2e51c6b
Jaybong <bongkot.klad@gmail.com>
2020-10-09 21:51:04 +0700

    Edit file config database

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
-    define("DB_HOST","us-cdbr-east-02.cleardb.com");
-    define("DB_USER","b1046991ac3a93");
-    define("DB_PASS","cf29f2ae");
-    define("DB_NAME","heroku_4fd12c739344d8d");
+    define("DB_HOST","localhost");
+    define("DB_USER","root");
+    define("DB_PASS","root");
+    define("DB_NAME","basic_crud");
 ?>
