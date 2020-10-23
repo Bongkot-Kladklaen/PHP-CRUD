62463494fdc8640fd7474057016aa56e361d9807
Jaybong <bongkot.klad@gmail.com>
2020-10-23 16:23:03 +0700

    edit sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE basic_crud CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-USE basdic_crud;
+USE basic_crud;
 CREATE TABLE tb_user(
     id int(11) not null AUTO_INCREMENT PRIMARY KEY,
     fname VARCHAR(100),
